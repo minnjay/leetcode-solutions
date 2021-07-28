a39a081eee79e75f2cf0030b175638fa341bf474
Jay Chok <47003230+minnjay@users.noreply.github.com>
2021-07-28 04:38:40 -0400

    Time: 52 ms (26.03%), Space: 15.4 MB (17.08%) - LeetHub

--- partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
+++ partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
@@ -1,16 +1,55 @@
 class Solution {
 public:
+    // recursive solution
+    // bool canPartitionKSubsets(vector<int>& nums, int k)
+    // {
+    //     int sum = accumulate(nums.begin(), nums.end(),0);
+    //     if(sum%k != 0)
+    //     {
+    //         return false;
+    //     }else
+    //     {
+    //         int targetSum = sum/k;
+    //         vector<bool> usedArr(nums.size(), false);
+    //         return canPartition(nums, 0, usedArr, k, 0, targetSum);
+    //     }
+    // }
+    
+    
+    // dynamic programming approach with bit masking
     bool canPartitionKSubsets(vector<int>& nums, int k)
     {
         int sum = accumulate(nums.begin(), nums.end(),0);
-        if(sum%k != 0 || k == 0)
+        if(sum%k != 0)
         {
             return false;
         }else
         {
-            int targetSum = sum/k;
-            vector<bool> usedArr(nums.size(), false);
-            return canPartition(nums, 0, usedArr, k, 0, targetSum);
+            sum = sum/k;
+            int n = nums.size();
+            vector<int> dp( (1<<n)+1 , -1);
+            dp[0] = 0;
+            
+            for(int i = 0; i< (1<<n); ++i)
+            {
+                if(dp[i] == -1)
+                {
+                    continue;
+                }
+                for(int j = 0; j<n; ++j)
+                {
+                    int isNot =  ( (i&(1<<j)) == 0 ) ; 
+                    if(isNot)
+                    {
+                        if(dp[i]+nums[j] <= sum)
+                        {
+                            int newSubset = i|(1<<j) ; 
+                            dp[newSubset] = (dp[i]+nums[j])==sum ? 0 : (dp[i]+nums[j]) ;  
+                        }
+                    }
+                }
+            }
+            return dp[(1<<n)-1] == 0;
         }
     }
     
