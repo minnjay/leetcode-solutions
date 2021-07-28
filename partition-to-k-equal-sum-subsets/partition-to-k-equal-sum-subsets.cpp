69c59e61ca81ebf58b7d523f3f555bf6f3187503
Jay Chok <47003230+minnjay@users.noreply.github.com>
2021-07-28 04:29:12 -0400

    Time: 732 ms (5.53%), Space: 9.1 MB (61.20%) - LeetHub

--- partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
+++ partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
@@ -2,21 +2,16 @@ class Solution {
 public:
     bool canPartitionKSubsets(vector<int>& nums, int k)
     {
-        int sum = 0;
-        for(int i = 0; i<nums.size(); ++i)
-        {
-            sum +=nums[i];
-        }
+        int sum = accumulate(nums.begin(), nums.end(),0);
         if(sum%k != 0 || k == 0)
         {
             return false;
         }else
         {
             int targetSum = sum/k;
-            vector<bool> usedArr(nums.size());
+            vector<bool> usedArr(nums.size(), false);
             return canPartition(nums, 0, usedArr, k, 0, targetSum);
         }
-        
     }
     
     bool canPartition(vector<int>& nums, int start, vector<bool>& used, int k, int progressSum, int targetSum)
@@ -25,14 +20,15 @@ public:
         {
             return true;
         }
-        if(progressSum == targetSum)
-        {
-            return canPartition(nums, 0,used, k-1,0, targetSum);
-        }
         if(progressSum > targetSum)
         {
             return false;
         }
+        if(progressSum == targetSum)
+        {
+            return canPartition(nums, 0,used, k-1,0, targetSum);
+        }
+        
         for(int i = start; i<nums.size(); ++i)
         {
             if(!used[i])
