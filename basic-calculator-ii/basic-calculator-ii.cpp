5679b2dcef5600534e1599a9feb527e865cb7de2
Jay Chok <47003230+minnjay@users.noreply.github.com>
2021-08-18 14:16:31 -0400

    Time: 8 ms (91.27%), Space: 8.5 MB (66.97%) - LeetHub

--- basic-calculator-ii/basic-calculator-ii.cpp
+++ basic-calculator-ii/basic-calculator-ii.cpp
@@ -2,85 +2,121 @@
 // public:
 //     int calculate(string s)
 //     {
-//         if(s.length() == 0)
+//         int len = s.length();
+//         if(len == 0)
 //         {
 //             return 0;
 //         }
-        
-//         int currentNum = 0;
-//         char currentOp ='+';
 //         stack<int> st;
-//         int answer = 0;
-        
-//         for(int i = 0; i<s.length(); ++i)
+//         int currentNum = 0;
+//         char currentOp = '+';
+//         int i = 0;
+//         string pro = "";
+//         for(int v = 0; v<len; ++v)
 //         {
-//             char cur = s[i];
+//             if(!iswspace(s[v]))
+//             {
+//                 pro += s[v];
+//             }
+//         }
+        
+        
+//         while(i<pro.length())
+//         {
+//             char cur = pro[i];
 //             if(isdigit(cur))
 //             {
-//                 currentNum = (currentNum*10) + ( cur -'0');
+//                 currentNum = (currentNum * 10) + ( cur - '0');
 //             }
-//             if(!isdigit(cur) && !iswspace(cur) || i == s.length()-1)
+            
+//             if(!isdigit(cur) || i == len-1)
 //             {
-//                 if(cur == '+')
+//                 if(cur == '-')
+//                 {
+//                     st.push( -currentNum );
+//                 }
+//                 else if(cur == '+')
 //                 {
 //                     st.push(currentNum);
 //                 }
-//                 else if(cur == '-')
-//                 {
-//                     st.push(-currentNum);
-//                 }
 //                 else if(cur == '*')
 //                 {
-//                     int temp = st.top();
-//                     st.pop();
-//                     st.push(temp*currentNum);
+//                     int track = i+1;
+//                     int anotherNum = 0;
+//                     while(isdigit(pro[track]))
+//                     {
+//                         anotherNum = (anotherNum * 10) + ( pro[track] - '0');
+//                         track ++;
+//                     }
+//                     st.push(currentNum*anotherNum);
+//                     i = track;
 //                 }
 //                 else if(cur == '/')
 //                 {
-//                     int temp = st.top();
-//                     st.pop();
-//                     st.push(temp/currentNum);
+//                     int track = i+1;
+//                     int anotherNum = 0;
+//                     while(isdigit(pro[track]))
+//                     {
+//                         anotherNum = (anotherNum * 10) + ( pro[track] - '0');
+//                         track ++;
+//                     }
+//                     st.push(currentNum/anotherNum);
+//                     i = track;
 //                 }
 //                 currentOp = cur;
 //                 currentNum = 0;
 //             }
+//             i++;
 //         }
+
+        
+//         int answer = 0;
 //         while(!st.empty())
 //         {
 //             answer += st.top();
 //             st.pop();
 //         }
 //         return answer;
-        
 //     }
 // };
+
 
 class Solution {
 public:
     int calculate(string s) {
-        int length = s.length();
-        if (length == 0) return 0;
-        int currentNumber = 0, lastNumber = 0, result = 0;
-        char sign = '+';
-        for (int i = 0; i < length; i++) {
+        int len = s.length();
+        if (len == 0) return 0;
+        stack<int> stack;
+        int currentNumber = 0;
+        char operation = '+';
+        for (int i = 0; i < len; i++) {
             char currentChar = s[i];
             if (isdigit(currentChar)) {
                 currentNumber = (currentNumber * 10) + (currentChar - '0');
             }
-            if (!isdigit(currentChar) && !iswspace(currentChar) || i == length - 1) {
-                if (sign == '+' || sign == '-') {
-                    result += lastNumber;
-                    lastNumber = (sign == '+') ? currentNumber : -currentNumber;
-                } else if (sign == '*') {
-                    lastNumber = lastNumber * currentNumber;
-                } else if (sign == '/') {
-                    lastNumber = lastNumber / currentNumber;
+            if (!isdigit(currentChar) && !iswspace(currentChar) || i == len - 1) {
+                if (operation == '-') {
+                    stack.push(-currentNumber);
+                } else if (operation == '+') {
+                    stack.push(currentNumber);
+                } else if (operation == '*') {
+                    int stackTop = stack.top();
+                    stack.pop();
+                    stack.push(stackTop * currentNumber);
+                } else if (operation == '/') {
+                    int stackTop = stack.top();
+                    stack.pop();
+                    stack.push(stackTop / currentNumber);
                 }
-                sign = currentChar;
+                operation = currentChar;
                 currentNumber = 0;
             }
         }
-        result += lastNumber;
-        return result;  
+        int result = 0;
+        while (stack.size() != 0) {
+            result += stack.top();
+            stack.pop();
+        }
+        return result;
     }
 };
